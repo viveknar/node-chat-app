581dd2e1864afe2d5f5d72217bbe7257d81516f6
Vivek <vivek.nar87@gmail.com>
2017-04-13 16:45:21 +0530

    Emit message on create message

--- public/js/index.js
+++ public/js/index.js
@@ -2,12 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'Vivek',
-        text: 'jello'
-    });
-
 });
 
 socket.on('newMessage', function (newMessage) {
--- server/server.js
+++ server/server.js
@@ -15,14 +15,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newMessage', {
-        from: 'anonymous',
-        text: 'Hello',
-        createdAt: 123
-    });
-
     socket.on('createMessage', (newMessage) => {
         console.log('New Message', newMessage);
+        io.emit('newMessage', {
+            from: newMessage.from,
+            text: newMessage.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
